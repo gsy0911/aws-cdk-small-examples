31645cdc357c0d20d1b5496e92ab5b178cc3cd07
yoshiki <yoshiki0911@gmail.com>
2020-11-05 23:59:37 +0900

    add lambda-layer example

---
+++ python/lambda-creating-lambda_layers/README.md
@@ -0,0 +1,6 @@
+# lambda-creating-lambda_layers
+
+
+# References
+
+* [Qiita](https://qiita.com/gsy0911/items/07e820a213d1e81fbe0d)
---
+++ python/lambda-creating-lambda_layers/lambda_script/__init__.py
@@ -0,0 +1,39 @@
+import subprocess
+import zipfile
+import boto3
+from pathlib import Path
+
+
+def lambda_handler(event, _):
+    # 引数の確認
+    if "package_list" not in event:
+        return {"error": "[package_list]が存在しません"}
+    package_list = event['package_list']
+    if type(package_list) is not list:
+        return {"error": "[package_list]がlistではありません"}
+    if "s3_bucket" not in event:
+        return {"error": "[s3_bucket]が存在しません"}
+    if "s3_key" not in event:
+        return {"error": "[s3_key]が存在しません"}
+
+    # pythonと言うディレクトリを作成し、そこにpackageをインストール
+    mkdir_cmd = "mkdir -p /tmp/python"
+    pip_install_cmd = f"pip install -t /tmp/python {' '.join(package_list)}"
+    process = (subprocess.Popen(f"{mkdir_cmd} && {pip_install_cmd}", stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+    print('commands...\n'+process)
+
+    # Pathオブジェクトを生成
+    p = Path("/tmp/python")
+
+    # zipにinstallしたpackageを追加
+    with zipfile.ZipFile('/tmp/python_lib.zip', 'w', compression=zipfile.ZIP_DEFLATED) as new_zip:
+        for f in list(p.glob("**/*")):
+            new_zip.write(f, arcname=str(f).replace("/tmp/", ""))
+
+    # upload to s3
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(event['s3_bucket'])
+    bucket.upload_file('/tmp/python_lib.zip', event['s3_key'])
+    return {
+        "success": f"s3://{event['s3_bucket']}/{event['s3_key']}に出力しました"
+    }
