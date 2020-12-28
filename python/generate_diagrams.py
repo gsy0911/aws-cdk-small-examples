53f10b8ec2ba3345b79b4616da060e5bb8a56800
yoshiki <yoshiki0911@gmail.com>
2020-12-28 23:39:38 +0900

    #13 feat: add new diagram

--- python/generate_diagrams.py
+++ python/generate_diagrams.py
@@ -18,7 +18,8 @@ from diagrams.aws.storage import (
     S3
 )
 from diagrams.aws.integration import (
-    StepFunctions
+    StepFunctions,
+    SQS
 )
 from diagrams.aws.analytics import (
     Glue
@@ -28,6 +29,9 @@ from diagrams.aws.management import (
 )
 from diagrams.aws.security import (
     IAMRole
+)
+from diagrams.aws.general import (
+    GenericSDK as SFn_TASK
 )
 
 
@@ -39,6 +43,25 @@ def apigw_dynamodb_lambda():
         apigw >> Edge(label="POST /example/update") >> Lambda("update status") >> Edge(label="update item") >> dynamodb
         apigw >> Edge(label="POST /example") >> Lambda("producer") >> Edge(label="put item") >> dynamodb
         apigw >> Edge(label="GET /example") >> Lambda("consumer") >> Edge(label="read all item") >> dynamodb
+
+
+def apigw_dynamodb_sfn_with_heavytask():
+    stack_objective = "apigw-dynamodb-sfn-with-heavytask"
+    with Diagram(stack_objective, outformat="png", filename=f"{stack_objective}/pics/arch", show=False):
+        sqs = SQS("SQS")
+        apigw = APIGateway("/task") >> Lambda("integration") >> [sqs, Dynamodb("DynamoDB")]
+
+        timer_lambda = Lambda("timer lambda")
+        sqs << Edge(label="dequeue") << timer_lambda << Cloudwatch("cron")
+
+        with Cluster(label="StepFunctions", direction="TB"):
+            sfn_start = SFn_TASK("update DynamoDB\nset `running`")
+            sfn_start \
+                >> Lambda("Some Task") \
+                >> [SFn_TASK("update DynamoDB\nset `success`"), SFn_TASK("update DynamoDB\nset `failure`")]
+
+        # invoke sfn from Lambda
+        timer_lambda >> sfn_start
 
 
 def apigw_lambda():
@@ -61,6 +84,7 @@ def batch_stepfunctions():
 
 def main():
     apigw_dynamodb_lambda()
+    apigw_dynamodb_sfn_with_heavytask()
     apigw_lambda()
     batch_stepfunctions()
 
